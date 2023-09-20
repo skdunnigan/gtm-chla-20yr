c178308254ae06d76765ff14bb397063cedecffc
Shannon Dunnigan Kimmel <Shannon.Dunnigan@dep.state.fl.us>
2023-09-20 13:54:22 -0400

    update packages

--- R/00_loadpackages.R
+++ R/00_loadpackages.R
@@ -4,17 +4,24 @@ library(readxl) # read excel files
 library(janitor) # simple tools to clean dirty data
 library(here) # a simpler way to find your files
 library(SWMPr) # working with SWMP data from the NERRS
-# library(SWMPrExtension) # expanded plotting and analysis of SWMP data from NERRS
-# library(xlsx) # to export df as .xlsx files
 
 # 02 tidy and wrangle ----
 library(tidyverse) # because...tidyverse (ggplot2, tidyr, dplyr)
 library(lubridate) # dates and times
+library(tidyr) # in case of package updates outside of `tidyverse`
+library(dplyr)
 
 # 03 pulling information and statistics ----
 library(broom) # convert statistical analysis objects into tidy tibbles
-library(EnvStats)
-library(psych)
+if(!require(wqtrends)){ 
+  options(repos = c(
+    tbeptech = 'https://tbep-tech.r-universe.dev',
+    CRAN = 'https://cloud.r-project.org'))
+  install.packages('wqtrends')} ; library(wqtrends) # models and plots for trends
+# if(!require(mgcv)){ install.packages("mgcv") } ;  library(mgcv) 
+# if(!require(changepoint)) { install.packages("changepoint")}; library(changepoint)
+# if(!require(strucchange)) { install.packages('strucchange')}; library(strucchange)
+# library(psych)
 # library(wql)
 
 # 04 markdown ----
