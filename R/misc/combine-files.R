2d4061c74388ca64c1043cda6f86a7e4965c3d29
Shannon Dunnigan Kimmel <Shannon.Dunnigan@dep.state.fl.us>
2023-09-21 16:35:57 -0400

    combine both data frames and export as .RData files

--- R/misc/combine-files.R
+++ R/misc/combine-files.R
@@ -2,6 +2,10 @@
 load(file = here('output', 'data', 'swmp.RData'))
 load(file = here('output', 'data', 'sjr.RData'))
 
+comb_dat <- bind_rows(sjr_dat, swmp_dat)
+save(comb_dat, file = here("output", "data", "comb.RData"))
+
+
 # check for pine island patterns
 
 bind_rows(sjr_dat, swmp_dat) %>% 
@@ -67,18 +71,19 @@ bind_rows(sjr_dat, swmp_dat) %>%
   theme(axis.title.x = element_blank()) +
   labs(x = "Chlorophyll-a (\U00B5g/L)")
 
+dat <- comb_dat %>% 
+  group_by(station, date) %>% # group by station and date to average duplicates
+  summarise(value = mean(value, na.rm = T)) %>% # avg duplicates
+  ungroup() %>%
+  mutate(station = toupper(station), # capitalize all stations
+         doy = lubridate::yday(date), # day of the year
+         cont_year = lubridate::decimal_date(date), # date in decimal time
+         yr = lubridate::year(date), # year
+         mo = lubridate::month(date, label = TRUE), # month
+         param = "chla") %>% # add param name
+  filter(yr > 2002 & yr < 2023) %>% # only keep data from 2003-2022 
+  filter(station != "MR312") # remove the SJR site near SS
 
+save(dat, file = here('output', 'data', 'dat.RData'))
 
-# prepping
-
-# group_by(station_code, date) %>% # group by station and date to average duplicates
-#   summarise(value = mean(chla_n, na.rm = T)) %>% # avg duplicates
-#   ungroup() %>% 
-#   rename(station = station_code) %>% # clean variable name
-#   filter(yr > 2002 & yr < 2023) # only keep data from 2003-2022
-# 
-# mutate(doy = lubridate::yday(date), # day of the year
-#        cont_year = lubridate::decimal_date(date), # date in decimal time
-#        yr = lubridate::year(date), # year
-#        mo = lubridate::month(date, label = TRUE), # month
-#        param = "chla") %>% # add param name
+rm(list = ls())
