72fa3106303fb0e7e79a73b033d96352d308a427
Shannon Dunnigan Kimmel <Shannon.Dunnigan@dep.state.fl.us>
2023-09-22 11:00:12 -0400

    export summary and fit stats for gams

--- R/misc/02_models.R
+++ R/misc/02_models.R
@@ -17,6 +17,9 @@ par(mfrow=c(2,2))
 gam.check(pi.mod)
 dev.off()
 
+pi.sum <- anlz_smooth(pi.mod) %>% mutate(station = "GTMPINUT")
+pi.fit <- anlz_fit(pi.mod) %>% mutate(station = "GTMPINUT")
+
 save(pi.mod, file = here('output', 'models', 'pi.mod.RData'))
 
 
@@ -33,6 +36,9 @@ par(mfrow=c(2,2))
 gam.check(j17.mod)
 dev.off()
 
+j17.sum <- anlz_smooth(j17.mod) %>% mutate(station = "JXTR17")
+j17.fit <- anlz_fit(j17.mod) %>% mutate(station = "JXTR17")
+
 save(j17.mod, file = here('output', 'models', 'j17.mod.RData'))
 
 # san sebastian -----------------------------------------------------------
@@ -47,6 +53,9 @@ pdf(file = here('output', 'models', 'ss-gam.check.pdf'))
 par(mfrow=c(2,2))
 gam.check(ss.mod)
 dev.off()
+
+ss.sum <- anlz_smooth(ss.mod) %>% mutate(station = "GTMSSNUT")
+ss.fit <- anlz_fit(ss.mod) %>% mutate(station = "GTMSSNUT")
 
 save(ss.mod, file = here('output', 'models', 'ss.mod.RData'))
 
@@ -63,6 +72,9 @@ par(mfrow=c(2,2))
 gam.check(j21.mod)
 dev.off()
 
+j21.sum <- anlz_smooth(j21.mod) %>% mutate(station = "JXTR21")
+j21.fit <- anlz_fit(j21.mod) %>% mutate(station = "JXTR21")
+
 save(j21.mod, file = here('output', 'models', 'j21.mod.RData'))
 
 
@@ -77,6 +89,9 @@ pdf(file = here('output', 'models', 'fm-gam.check.pdf'))
 par(mfrow=c(2,2))
 gam.check(fm.mod)
 dev.off()
+
+fm.sum <- anlz_smooth(fm.mod) %>% mutate(station = "GTMFMNUT")
+fm.fit <- anlz_fit(fm.mod) %>% mutate(station = "GTMFMNUT")
 
 save(fm.mod, file = here('output', 'models', 'fm.mod.RData'))
 
@@ -93,6 +108,9 @@ par(mfrow=c(2,2))
 gam.check(mrt.mod)
 dev.off()
 
+mrt.sum <- anlz_smooth(mrt.mod) %>% mutate(station = "MRT")
+mrt.fit <- anlz_fit(mrt.mod) %>% mutate(station = "MRT")
+
 save(mrt.mod, file = here('output', 'models', 'mrt.mod.RData'))
 
 
@@ -108,7 +126,28 @@ par(mfrow=c(2,2))
 gam.check(pc.mod)
 dev.off()
 
+pc.sum <- anlz_smooth(pc.mod) %>% mutate(station = "GTMPCNUT")
+pc.fit <- anlz_fit(pc.mod) %>% mutate(station = "GTMPCNUT")
+
 save(pc.mod, file = here('output', 'models', 'pc.mod.RData'))
+
+sum <- bind_rows(pi.sum,
+                  j17.sum,
+                  ss.sum,
+                  j21.sum,
+                  fm.sum,
+                  mrt.sum,
+                  pc.sum) %>% select(station, 1:5)
+fit <- bind_rows(pi.fit,
+                 j17.fit,
+                 ss.fit,
+                 j21.fit,
+                 fm.fit,
+                 mrt.fit,
+                 pc.fit) %>% select(station, 1:3)
+
+write.csv(sum, file = here('output', 'models', 'gam.summary.csv'))
+write.csv(fit, file = here('output', 'models', 'gam.fit.csv'))
 
 dev.off()
 rm(list = ls())
