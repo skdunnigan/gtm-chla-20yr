27f28ff5d484af21939d0b03b45ba06442b7fafd
Shannon Dunnigan Kimmel <Shannon.Dunnigan@dep.state.fl.us>
2023-10-24 14:55:52 -0400

    seasonal trend testing added with plots

--- R/misc/03_create-gam-plots.R
+++ R/misc/03_create-gam-plots.R
@@ -150,4 +150,145 @@ c <- show_prddoy(mrt.mod, ylab = ylab) + labs(title = "MRT")
 print((a + labs(x = "")) / (b + theme(legend.position = "none") + labs(x = "")) / (c + theme(legend.position = "none"))) %>% ggsave(filename = here('output', 'figures', 'trends', 'doy-matanzas.png'), height = 11, width = 8.5, units = "in")
 
 
+
+# gam-plots for spring/fall comparisons -----------------------------------
+
+show_met2 <- function (mod, metfun = mean, doystr = 1, doyend = 364, yrstr = 2000, 
+                       yrend = 2019, yromit = NULL, ylab, nsim = 10000, useave = FALSE, 
+                       base_size = 11, xlim = NULL, ylim = NULL, ...) 
+{
+  chk <- identical(deparse(metfun), deparse(mean))
+  if (!chk & useave) 
+    stop("Specify metfun = mean if useave = T")
+  if (useave) 
+    metseason <- anlz_avgseason(mod, doystr = doystr, doyend = doyend)
+  if (!useave) 
+    metseason <- anlz_metseason(mod, metfun, doystr = doystr, 
+                                doyend = doyend, nsim = nsim, ...)
+  if (!is.null(yromit)) 
+    metseason <- metseason %>% dplyr::filter(!yr %in% yromit)
+  trans <- mod$trans
+  dts <- as.Date(c(doystr, doyend), origin = as.Date("2000-12-31"))
+  strt <- paste(lubridate::month(dts[1], label = T, abbr = T))
+  ends <- paste(lubridate::month(dts[2], label = T, abbr = T))
+  func <- as.character(substitute(metfun))
+  ttl <- NULL
+  subttl <- NULL
+  toplo1 <- metseason
+  p <- ggplot2::ggplot(data = toplo1, ggplot2::aes(x = yr, 
+                                                   y = bt_met)) + ggplot2::geom_point(colour = "deepskyblue3") + 
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = bt_lwr, ymax = bt_upr), 
+                           colour = "deepskyblue3") + ggplot2::theme_bw(base_family = "serif", 
+                                                                        base_size = base_size) + ggplot2::theme(axis.title.x = ggplot2::element_blank())
+  if (!any(is.null(yrstr) | is.null(yrend))) {
+    mixmet <- anlz_mixmeta(metseason, yrstr = yrstr, yrend = yrend, 
+                           yromit = yromit)
+    toplo2 <- data.frame(yr = seq(yrstr, yrend, length = 50)) %>% 
+      dplyr::mutate(met = predict(mixmet, newdata = data.frame(yr = yr)), 
+                    se = predict(mixmet, newdata = data.frame(yr = yr), 
+                                 se = T)[, 2], bt_lwr = met - 1.96 * se, bt_upr = met + 
+                      1.96 * se, bt_met = met)
+    pval <- coefficients(summary(mixmet)) %>% data.frame %>% 
+      .[2, 4] %>% anlz_pvalformat()
+    if (mod$trans == "log10") {
+      dispersion <- summary(mod)$dispersion
+      toplo2 <- data.frame(yr = seq(yrstr, yrend, length = 50)) %>% 
+        dplyr::mutate(met = predict(mixmet, newdata = data.frame(yr = yr)), 
+                      se = predict(mixmet, newdata = data.frame(yr = yr), 
+                                   se = T)[, 2], bt_lwr = 10^((met - 1.96 * 
+                                                                 se) + log(10) * dispersion/2), bt_upr = 10^((met + 
+                                                                                                                1.96 * se) + log(10) * dispersion/2), bt_met = 10^(met + 
+                                                                                                                                                                     log(10) * dispersion/2))
+      slope <- lm(bt_met ~ yr, toplo2) %>% summary %>% 
+        coefficients %>% .[2, 1]
+      slope <- round(slope, 2)
+      logslope <- summary(mixmet)$coefficients[2, c(1, 
+                                                    5, 6)]
+      logslope <- round(logslope, 2)
+      logslope <- paste0(logslope[1], " (", logslope[2], 
+                         ", ", logslope[3], ")")
+      subttl <- paste0(yrstr, "-", yrend, ", ", strt, "-", ends, ", ", logslope, ", ", pval)
+    }
+    if (mod$trans == "ident") {
+      slope <- summary(mixmet)$coefficients[2, c(1, 5, 
+                                                 6)]
+      slope <- round(slope, 2)
+      slope <- paste0(slope[1], " (", slope[2], ", ", slope[3], 
+                      ")")
+      subttl <- paste0(yrstr, "-", yrend, ", ", strt, "-", ends, ", ",
+                       slope, ", ", logslope, ", ", pval)
+    }
+    p <- p + ggplot2::geom_ribbon(data = toplo2, ggplot2::aes(ymin = bt_lwr, 
+                                                              ymax = bt_upr), fill = "pink", alpha = 0.4) + ggplot2::geom_line(data = toplo2, 
+                                                                                                                               color = "pink")
+  }
+  p <- p + ggplot2::labs(title = ttl, subtitle = subttl, y = ylab) + 
+    ggplot2::coord_cartesian(xlim = xlim, ylim = ylim)
+  return(p)
+}
+
+
+plot4 <- function(mod, site, save) {
+  ylab <- "Chlorophyll-a (\U00B5g/L)"
+  
+  site <- paste(site)
+  filename <- paste0(site,"snl-trend.plots.png")
+  
+  a <- show_met2(mod, doystr = 1, doyend = 181, yrstr = 2003, yrend = 2012, ylab = ylab)
+  b <- show_met2(mod, doystr = 1, doyend = 181, yrstr = 2013, yrend = 2022, ylab = ylab) + labs(y = "")
+  c <- show_met2(mod, doystr = 1, doyend = 181, yrstr = 2018, yrend = 2022, ylab = ylab) + labs(y = "")
+  
+  d <- show_met2(mod, doystr = 182, doyend = 365, yrstr = 2003, yrend = 2012, ylab = ylab) 
+  e <- show_met2(mod, doystr = 182, doyend = 365, yrstr = 2013, yrend = 2022, ylab = ylab) + labs(y = "")
+  f <- show_met2(mod, doystr = 182, doyend = 365, yrstr = 2018, yrend = 2022, ylab = ylab) + labs(y = "")
+  
+  g <- (a + b + c) / (d + e + f) + 
+    plot_annotation(tag_levels = "a") & theme(plot.tag = element_text(size = 12))
+  
+  if (save == TRUE) {
+    ggsave(g, filename = here('output', 'figures', 'trends', filename))
+  }
+  else {
+    print(g)
+  }
+  
+}
+
+plot4(mod = pi.mod, site = "GTMPINUT", save = T)
+plot4(mod = j17.mod, site = "JXTR17", save = T)
+plot4(mod = ss.mod, site = "GTMSSNUT", save = T)
+plot4(mod = j21.mod, site = "JXTR21", save = T)
+plot4(mod = fm.mod, site = "GTMFMNUT", save = T)
+plot4(mod = mrt.mod, site = "MRT", save = T)
+plot4(mod = pc.mod, site = "GTMPCNUT", save = T)
+
+
+plot5 <- function(mod, site, save) {
+  ylab <- "Chlorophyll-a (\U00B5g/L)"
+  
+  site <- paste(site)
+  filename <- paste0(site,"20snl-trend.plots.png")
+  
+  a <- show_met2(mod, doystr = 1, doyend = 181, yrstr = 2003, yrend = 2022, ylab = ylab)
+  b <- show_met2(mod, doystr = 182, doyend = 365, yrstr = 2003, yrend = 2022, ylab = ylab) 
+
+  c <- a / b 
+
+  if (save == TRUE) {
+    ggsave(c, filename = here('output', 'figures', 'trends', filename))
+  }
+  else {
+    print(c)
+  }
+  
+}
+
+plot5(mod = pi.mod, site = "GTMPINUT", save = T)
+plot5(mod = j17.mod, site = "JXTR17", save = T)
+plot5(mod = ss.mod, site = "GTMSSNUT", save = T)
+plot5(mod = j21.mod, site = "JXTR21", save = T)
+plot5(mod = fm.mod, site = "GTMFMNUT", save = T)
+plot5(mod = mrt.mod, site = "MRT", save = T)
+plot5(mod = pc.mod, site = "GTMPCNUT", save = T)
+
 rm(list = ls())
