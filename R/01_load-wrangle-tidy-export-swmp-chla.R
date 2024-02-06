abf6d54935165a1acb81bd997a769ce44609da03
Shannon Dunnigan Kimmel <Shannon.Dunnigan@dep.state.fl.us>
2024-02-06 13:41:45 -0500

    rename and reorganize export code for chla data

--- R/01_load-wrangle-tidy-export-swmp-chla.R
+++ R/01_load-wrangle-tidy-export-swmp-chla.R
@@ -1,4 +1,10 @@
-# create .Rdata object of chl-a data from GTMNERR SWMP
+# read in GTMNERR SWMP nutrient data, tidy, and export as swmp.Rdata
+
+library(here)
+source(here('R', '00_loadpackages.R'))
+
+
+# load nutrient data ------------------------------------------------------
 
 nms <- names(read_excel(here::here('data',
                                    'SWMP',
@@ -24,6 +30,13 @@ rm(nms, class, class2)
 
 glimpse(NUT)
 
+
+# wrangle and tidy --------------------------------------------------------
+
+# only keep chlorophyll data
+# clean up variable names, convert datetime into a date (only sampled monthly), keep only grab samples (remove diel), 
+# only keep essential columns: station code, f_record, chla_n, f_chla_n, and date
+
 chla <- NUT %>% 
   filter(!is.na(rep)) %>% # remove "S" reps in dataset
   select(station_code, 2:5, chla_n, f_chla_n) %>% # keep only chla data
@@ -32,7 +45,10 @@ chla <- NUT %>%
   filter(monitoring_program == 1) %>% # keep only grab samples (remove DIEL)
   select(-monitoring_program, -rep, -datetimestamp) # remove columns once done
 
-chla$chla_n[chla$chla_n < 0.55] <- 0.55 # replace all values below nominal base mdl of 0.55 with the base mdl 0.55
+# replace all values below nominal base mdl of 0.55 with the base mdl 0.55
+chla$chla_n[chla$chla_n < 0.55] <- 0.55 
+
+# filter data to remove data that is flagged rejected and suspect
 
 chla_dat <- chla %>% 
   filter(!grepl(c("<-3>"), f_chla_n) & 
@@ -41,7 +57,8 @@ chla_dat <- chla %>%
   select(-f_record, -f_chla_n) %>%  # remove qc columns after filtering
   rename(value = chla_n)
 
-# using ISCO data to fill in gaps in PC  
+# use data from the diel sampling at PC to fill in gaps in PC monthly grab sample
+# all data were collected within one hour of the original grab sample
 
 pc2013 <- NUT %>% 
   filter(!is.na(rep)) %>% # remove "S" reps in dataset
@@ -78,14 +95,21 @@ pc2022 <- NUT %>%
   select(station_code, date, chla_n) %>% 
   rename(value = chla_n)
 
+# combine all PC diel sampler-related data 
 pcISCO <- bind_rows(pc2013, pc2018, pc2022)
 
-swmp_dat <- bind_rows(chla_dat, pcISCO) %>% 
-  rename(station = station_code) %>% 
-  group_by(station, date) %>%  
-  summarize(value = mean(value, na.rm = T)) %>%
-  ungroup()
+# merge PC diel data with entire chla series
+
+swmp_dat <- bind_rows(chla_dat, pcISCO) %>% # combine data frames with the missing PC values
+  rename(station = station_code) %>% # rename station code for ease
+  group_by(station, date) %>%  # group by station and date
+  summarize(value = mean(value, na.rm = T)) %>% # average all monthly replicates together for one monthly value
+  ungroup() # ungroup dataset
+
+
+# export as .RData file ---------------------------------------------------
 
 save(swmp_dat, file = here("output", "data", "swmp.RData"))
 
+# clear environment
 rm(list = ls())
